36e054509fb9a97ff2b5c19d52e1c3c15fd7f008
Robert Snyder <r.snyder49@gmail.com>
2018-12-28 00:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ function handleSubmit() {
  
   document.getElementById("main").innerHTML = template(recipe);
 }
+
+function displayEditForm() {
+  let name = document.getElementById("name").innerHTML;
+  let description = document.getElementById("description").innerHTML;
+  let ingredNodes = document.getElementsByName("ingredients");
+  let ingredients = [];
+ 
+  for (let i = 0; i < ingredNodes.length; i++) {
+     ingredients.push(ingredNodes[i].innerHTML);
+  }
+ 
+  let recipe = { name, ingredients, description, 'true': 'handleSubmit()'};
+  let recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML;
+  let template = Handlebars.compile(recipeFormTemplate);
+ 
+  document.getElementById("main").innerHTML = template(recipe);
+}
+ 
+ 
+ 
+ 
