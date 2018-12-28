07a5743f91fc3b04db04f3141993aefb3cfe0e73
Robert Snyder <r.snyder49@gmail.com>
2018-12-28 01:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function init() {
-  let formTemplate = document.getElementById("recipe-form-template").innerHTML;
-  let template = Handlebars.compile(formTemplate);
-}
+   let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+   document.getElementsByTagName("main")[0].innerHTML = template({"submitAction": "handleSubmit()"});
+   Handlebars.registerPartial("recipeDetailsPartial", document.getElementById("recipe-details-partial").innerHTML);
+   Handlebars.registerHelper("displayIngredient", function(ingredient) {
+     return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>');
+   });
+ }	 
 
 document.getElementsByTagName("main")[0].innerHTML = template({'true': 'handleSubmit()'});
 
